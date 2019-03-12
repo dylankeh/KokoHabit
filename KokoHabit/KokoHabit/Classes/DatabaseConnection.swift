dbde5a6040f620c200a8f92e69d370efed3d681b
dennissuarez <suareden@sheridancollege.ca>
2019-03-11 20:18:08 -0400

    Creating a connection to a Database and importing the webKit + sqlite3.0

---
+++ KokoHabit/KokoHabit/Classes/DatabaseConnection.swift
@@ -0,0 +1,28 @@
+//Name: Dennis Suarez
+//Date: March 11, 2019
+//
+
+import UIKit
+
+class DatabaseConnection: NSObject {
+    
+    private var docsDir : String?
+    private var databasePath : String?
+    private var db : OpaquePointer? = nil
+    
+    //Initializing basic variables
+    override init() {
+        
+        super.init()
+        let docsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        docsDir = docsPath[0]
+    }
+    
+    //Returns a copy of the database path
+    public func getDataBasePath(databaseName : String) -> String?{
+        
+        databasePath = docsDir?.appending("/" + databaseName)
+        return databasePath
+    }
+
+}

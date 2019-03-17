96b4332cf3a1273907efb29fa3084f2420a86f02
dennissuarez <suareden@sheridancollege.ca>
2019-03-17 19:04:46 -0400

    I added the sign up page and login page(No functionality)

---
+++ KokoHabit/KokoHabit/ViewControllers/MainController.swift
@@ -0,0 +1,23 @@
+//
+//  MainController.swift
+//  KokoHabit
+//
+//  Created by Dennis Suarez on 2019-03-17.
+//  Copyright © 2019 koko. All rights reserved.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unWindToHome(sender: UIStoryboardSegue) {
+        
+    }
+
+}

2decf3aa92d451db21358f138ae283f98d9b863e
dennissuarez <suareden@sheridancollege.ca>
2019-03-23 13:58:50 -0400

    Adding functionality for inserting

--- KokoHabit/KokoHabit/Classes/DAO.swift
+++ KokoHabit/KokoHabit/Classes/DAO.swift
@@ -10,14 +10,32 @@ class DAO: NSObject {
     
     private let databaseCheck = DatabaseConnection();
     private var databasePath: String?
+    private var db: OpaquePointer? = nil
+    
+    let delegate = UIApplication.shared.delegate as! AppDelegate
     
     override init() {
         super.init()
         databasePath = databaseCheck.getDataBasePath(databaseName: "IOSProject.db")
     }
     
-    public func addPerson() {
-        //Code
+    public func addPerson(email:NSString, name:NSString, age:Int32, password:NSString, occupation:NSString) {
+        let addPerson = "INSERT INTO user (email, name, age, password, occupation) VALUES (?,?,?,?,?)"
+        
+        if validator(){
+            var sqlQuery: OpaquePointer? = nil
+            if sqlite3_prepare_v2(db, addPerson, -1 , &sqlQuery, nil) == SQLITE_OK{
+                sqlite3_bind_text(sqlQuery, 1, email.utf8String, -1, nil)
+                sqlite3_bind_text(sqlQuery, 2, name.utf8String, -1, nil)
+                sqlite3_bind_int(sqlQuery, 3, age)
+                sqlite3_bind_text(sqlQuery, 4, password.utf8String, -1, nil)
+                sqlite3_bind_text(sqlQuery, 5, occupation.utf8String, -1, nil)
+                
+                if sqlite3_step(sqlQuery) == SQLITE_DONE {
+                    print("Successful insertion")
+                }
+            }
+        }
     }
     
     public func deletePerson(email:String) {
@@ -42,6 +60,10 @@ class DAO: NSObject {
     
     public func viewHabits(email:String) {
         //Code
+    }
+    
+    private func validator()->Bool {
+        return sqlite3_open(self.databasePath, &db) == SQLITE_OK
     }
     
 }

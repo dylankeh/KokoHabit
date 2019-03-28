f243b9ff90db851e3b2d890b7067bed16d89835f
Emerald Liang <lianxiao@sheridancollege.ca>
2019-03-28 18:01:43 -0400

    Add HabitCell

---
+++ KokoHabit/KokoHabit/Classes/HabitCell.swift
@@ -0,0 +1,24 @@
+//
+//  HabitCell.swift
+//  KokoHabit
+//
+//  Created by Xiaoyu Liang on 2019/3/28.
+//  Copyright © 2019 koko. All rights reserved.
+//
+
+import UIKit
+
+class HabitCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

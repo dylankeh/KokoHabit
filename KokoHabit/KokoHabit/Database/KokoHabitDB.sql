a6008ec18c73370949760eea5f85a5b42e9f0e37
Arthur Tran <trankhoa@sheridancollege.ca>
2019-03-31 19:14:07 -0400

    KokoHabitDB: update tables and add triggers

--- KokoHabit/KokoHabit/Database/KokoHabitDB.sql
+++ KokoHabit/KokoHabit/Database/KokoHabitDB.sql
@@ -19,6 +19,7 @@ CREATE TABLE habit (
     email TEXT NOT NULL,
     pointValue INTEGER NOT NULL,
     name TEXT NOT NULL,
+    active BOOLEAN DEFAULT(TRUE), 
     FOREIGN KEY (email) REFERENCES user (email)
     ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -41,7 +42,6 @@ CREATE TABLE week_habit (
 
 CREATE TABLE day (
     date PRIMARY KEY,
-    pointsAchieved INTEGER NOT NULL DEFAULT(0),
     weekStartDate DATE,
     FOREIGN KEY (weekStartDate) REFERENCES week (weekStartDate)
 );
@@ -56,27 +56,64 @@ CREATE TABLE day_habit (
 	FOREIGN KEY (habitId) REFERENCES habit (id)
 );
 
-INSERT INTO user VALUES ("koko","koko",22,"",NULL);
+-- automatically insert all active habits into week_habit table when a new week is added
+CREATE TRIGGER insert_into_week_habit
+	AFTER INSERT ON week
+BEGIN
+	INSERT INTO week_habit SELECT NEW.weekStartDate, id FROM habit WHERE active=1;
+END;
 
-INSERT INTO habit VALUES (1, "koko", 20, "Eat healthy");
-INSERT INTO habit VALUES (2, "koko", 40, "Go to gym");
-INSERT INTO habit VALUES (3, "koko", 10, "Read a book");
-INSERT INTO habit VALUES (4, "koko", 30, "Wake up early");
+-- automatically insert the weeks habit into day_habit table when a new day is added
+CREATE TRIGGER insert_into_day_habit
+	AFTER INSERT ON day
+BEGIN
+	INSERT INTO day_habit SELECT NEW.date, wh.habitId, h.pointValue, 0 
+	    FROM week_habit wh INNER JOIN  habit h ON wh.habitId = h.id
+	    WHERE wh.weekStartDate=NEW.weekStartDate;
+END;
+
+-- set habit to inactive and delete from day_habit when user deletes habit from their weekly habits
+CREATE TRIGGER set_habit_to_inactive
+	AFTER DELETE ON week_habit
+BEGIN
+	UPDATE habit SET active=0 WHERE id=OLD.habitId;
+	DELETE FROM day_habit WHERE habitId=OLD.habitId;
+END;
+
+-- automatically insert into day_habit and week_habit table when new habit is added
+CREATE TRIGGER add_habit_to_dayweek_habit
+    AFTER INSERT ON habit
+BEGIN
+    INSERT INTO week_habit SELECT MAX(weekStartDate), NEW.id FROM week;
+    INSERT INTO day_habit SELECT MAX(date), NEW.id, NEW.pointValue, 0 FROM day;
+END;
+
+
+INSERT INTO user VALUES ("koko","koko",22,"1234","Student");
+
+INSERT INTO habit VALUES (1, "koko", 20, "Eat healthy",1);
+INSERT INTO habit VALUES (2, "koko", 40, "Go to gym",1);
+INSERT INTO habit VALUES (3, "koko", 10, "Read a book",1);
+INSERT INTO habit VALUES (4, "koko", 30, "Wake up early",1);
+INSERT INTO habit VALUES (5, "koko", 20, "Sleep Early",1);
 
 INSERT INTO coupon VALUES (NULL, "koko", 50);
 
 INSERT INTO week VALUES ("2019-03-24","2019-03-30",1,60,500);
--- INSERT INTO week VALUES ("2019-03-31","2019-04-06",0,60,0);
+INSERT INTO week VALUES ("2019-03-31","2019-04-06",0,60,0);
 
-INSERT INTO week_habit VALUES ("2019-03-24", 1);
-INSERT INTO week_habit VALUES ("2019-03-24", 2);
-INSERT INTO week_habit VALUES ("2019-03-24", 3);
-INSERT INTO week_habit VALUES ("2019-03-24", 4);
+INSERT INTO day VALUES ("2019-03-30","2019-03-24");
+INSERT INTO day VALUES ("2019-03-31","2019-03-31");
 
-INSERT INTO day VALUES ("2019-03-30",70,"2019-03-24");
+-- INSERT INTO week_habit VALUES ("2019-03-24", 1);
+-- INSERT INTO week_habit VALUES ("2019-03-24", 2);
+-- INSERT INTO week_habit VALUES ("2019-03-24", 3);
+-- INSERT INTO week_habit VALUES ("2019-03-24", 4);
 
-INSERT INTO day_habit VALUES ("2019-03-30",1,20,1);
-INSERT INTO day_habit VALUES ("2019-03-30",2,40,1);
-INSERT INTO day_habit VALUES ("2019-03-30",3,10,1);
-INSERT INTO day_habit VALUES ("2019-03-30",4,30,0);
+INSERT INTO day VALUES ("2019-03-30", "2019-03-24");
+
+-- INSERT INTO day_habit VALUES ("2019-03-30",1,20,1);
+-- INSERT INTO day_habit VALUES ("2019-03-30",2,40,1);
+-- INSERT INTO day_habit VALUES ("2019-03-30",3,10,1);
+-- INSERT INTO day_habit VALUES ("2019-03-30",4,30,0);
 

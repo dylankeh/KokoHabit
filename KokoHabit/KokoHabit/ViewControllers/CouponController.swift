21459c8efab25d6b9add6cbd804b78b246e71fb4
Emerald Liang <lianxiao@sheridancollege.ca>
2019-04-02 18:32:49 -0400

    Add Coupon View Controller

---
+++ KokoHabit/KokoHabit/ViewControllers/CouponController.swift
@@ -0,0 +1,16 @@
+//
+//  CouponController.swift
+//  KokoHabit
+//
+//  Created by Xiaoyu Liang on 2019/4/2.
+//  Copyright © 2019 koko. All rights reserved.
+//
+
+import UIKit
+
+class CouponController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

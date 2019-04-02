4ff27f5e57b5b8c9dc7ed27a3606125cc205050a
Emerald Liang <lianxiao@sheridancollege.ca>
2019-04-02 18:33:56 -0400

    Add table view for coupon page, link table cell

---
+++ KokoHabit/KokoHabit/Classes/CouponCell.swift
@@ -0,0 +1,23 @@
+//
+//  CouponCell.swift
+//  KokoHabit
+//
+//  Created by Xiaoyu Liang on 2019/4/2.
+//  Copyright © 2019 koko. All rights reserved.
+//
+
+import UIKit
+
+class CouponCell: UITableViewCell {
+
+    @IBOutlet weak var lblCoupon: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}

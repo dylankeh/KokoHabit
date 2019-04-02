aeb53c0c137d7c7ca4b0289f422f02904113580d
Emerald Liang <lianxiao@sheridancollege.ca>
2019-04-02 19:48:11 -0400

    Format table cell

--- KokoHabit/KokoHabit/Classes/CouponCell.swift
+++ KokoHabit/KokoHabit/Classes/CouponCell.swift
@@ -11,6 +11,17 @@ import UIKit
 class CouponCell: UITableViewCell {
 
     @IBOutlet weak var lblCoupon: UILabel!
+    @IBOutlet weak var lblCouponValue: UILabel!
+    
+    func setCoupon(coupon: Coupon, index: Int) {
+        lblCoupon.text = "Coupon \(index)"
+        lblCouponValue.text = "\(String(coupon.getPointValued())) points"
+    }
+    
+    func setUsedCoupon(){
+        lblCoupon.textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        lblCouponValue.textColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
--- KokoHabit/KokoHabit/ViewControllers/CouponController.swift
+++ KokoHabit/KokoHabit/ViewControllers/CouponController.swift
@@ -8,9 +8,27 @@
 
 import UIKit
 
-class CouponController: UIViewController {
+class CouponController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let dao = DAO()
+    let delegate = UIApplication.shared.delegate as! AppDelegate
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return delegate.coupons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CouponCell") as! CouponCell
+        cell.setCoupon(coupon: delegate.coupons[indexPath.row],index: indexPath.row+1)
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        dao.getCoupons()
     }
 }

2a0e148260cc1fe2542fc33eb4c8219e8ada3163
Arthur Tran <trankhoa@sheridancollege.ca>
2019-04-13 10:13:54 -0400

    DateCell: add comment explaining purpose

--- KokoHabit/KokoHabit/Classes/DateCell.swift
+++ KokoHabit/KokoHabit/Classes/DateCell.swift
@@ -2,6 +2,9 @@
 //  CustomCell.swift
 //  KokoHabit
 //
+//  This cell is used to show a cell for the month calendar
+//  Which only shows the day of the month number
+//
 //  Created by Khoa Tran on 2019-04-02.
 //  Copyright © 2019 koko. All rights reserved.
 //
